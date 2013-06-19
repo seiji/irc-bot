26262ed51289d28370c1e7848122ebee04443bdc
Seiji Toyama <seijit@me.com>
2013-06-19 12:54:49 +0900

    Create daemontools directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,29 +30,62 @@ role :db,   host, :primary => true
 
 set :bot_name, "seijit" if !
 
+# daemontools
+set :svscan_root, "/service"
+set :supervise_name, "#{application}-#{bot_name}"
+set :svname, "#{svscan_root}/.#{supervise_name}" 
+set :acct_name, "logadmin"
+set :acct_group, "logadmin"
+
+def svc(cmd)
+  sudo "svc #{cmd} #{svname}"
+end
+
 namespace :deploy do
   task :start, :roles => :app do
-    svc('-u', bot_name)
+    svc('-u')
   end
 
   task :stop, :roles => :app do
-    svc('-d', bot_name)
+    svc('-d')
   end
 
   task :restart, :roles => :app do
-    svc('-h', bot_name)
+    svc('-h')
   end
 
   task :setup_config, roles: :app do
-    sudo "ln -fs #{current_path}/service/#{application}-#{bot_name} /service/#{application}-#{bot_name}"
+    sudo "mkdir    #{svname}"
+    sudo "chmod +t #{svname}"
+    sudo "mkdir    #{svname}/log"
+    sudo "mkdir    #{svname}/log/main"
+    sudo "touch    #{svname}/log/status"
+    sudo "chown #{acct_name}:#{acct_group} #{svname}/log/main"
+    sudo "chown #{acct_name}:#{acct_group} #{svname}/log/status"
+
+    run_script = <<-EOS
+#!/bin/sh
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
+export PATH
+
+exec 2>&1
+sleep 3
+
+EOS
+    sudo "cat #{run_script} >#{svname}/run"
+    sudo "chmod +x #{svname}/run"
+
+    run_log_script = <<-EOS
+#!/bin/sh
+exec setuidgid ${acct_name} multilog t s1000000 n100 ./main
+
+EOS
+    sudo "cat #{run_log_script} >#{svname}/log/run"
+    sudo "chmod +x #{svname}/log/run"
+    
+#    sudo "ln -fs #{current_path}/service/#{application}-#{bot_name} /service/#{application}-#{bot_name}"
   end
   after "deploy:setup", "deploy:setup_config"
 end
 
-# daemontools
-set :svscan_root, "/service"
-set :supervise_name, "#{application}"
-
-def svc(cmd, name)
-  sudo "svc #{cmd} #{svscan_root}/#{supervise_name}-#{name}"
-end
